7ae43ad2150ed54ea7b4f4e4caffab9e717663e4
bibek <crazy764gaming@gmail.com>
2026-01-22 13:58:56 +0530

    Update suduco_solve.py

--- suduco_solve.py
+++ suduco_solve.py
@@ -79,7 +79,7 @@ def solve_sudoku(matrix, size):
 
 def solution(matrix, size):
     if solve_sudoku(matrix, size):
-        print("\nSolved Sudoku:")
+        print("\nSolved Sudoku:x")
         print(matrix)
     else:
         print("\nNo solution exists for this puzzle.")
